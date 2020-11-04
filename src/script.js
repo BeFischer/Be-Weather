7f524dd08aa4ed3cb60ceeed09098353c77021ae
Be Fischer <be_fischer@yahoo.com>
2020-11-04 09:18:09 -0500

    pull weather by long/lat ...working on the c/f conversion

--- src/script.js
+++ src/script.js
@@ -58,27 +58,27 @@ return `${direction}`;
 
 //Search for and return city
 
-function showTemperature(response) {
+//function showTemperature(response) {
   //let wDirection = response.data.wind.deg;
   //if wDirection 
-  console.log(response.data.wind);
-  tempF = response.data.main.temp;
-  tempFfeelsLike=response.data.main.feels_like;
-  document.querySelector(".currentTemp").innerHTML = `${(Math.round(tempF))}`;
-  document.querySelector(".currentCity").innerHTML = response.data.name;
-  document.querySelector(".humidity").innerHTML = `${response.data.main.humidity}% Humidity`;
-  document.querySelector("#feelsLike").innerHTML = `${Math.round(response.data.main.feels_like)}`;
-  document.querySelector(".sky").innerHTML =response.data.weather[0].description;
-  document.querySelector(".sun").innerHTML =`Sunrise:${formatHours(response.data.sys.sunrise*1000)} Sunset:${formatHours(response.data.sys.sunset*1000)}`;
+  //console.log(response.data.wind);
+  //tempF = response.data.main.temp;
+  //tempFfeelsLike=response.data.main.feels_like;
+  //document.querySelector(".currentTemp").innerHTML = `${(Math.round(tempF))}`;
+  //document.querySelector(".currentCity").innerHTML = response.data.name;
+  //document.querySelector(".humidity").innerHTML = `${response.data.main.humidity}% Humidity`;
+  //document.querySelector("#feelsLike").innerHTML = `${Math.round(response.data.main.feels_like)}`;
+  //document.querySelector(".sky").innerHTML =response.data.weather[0].description;
+  //document.querySelector(".sun").innerHTML =`Sunrise:${formatHours(response.data.sys.sunrise*1000)} Sunset:${formatHours(response.data.sys.sunset*1000)}`;
 
-  document.querySelector(".wind").innerHTML =`${wDirection(response.data.wind.deg)} Wind:${Math.round(response.data.wind.speed)}mph`
+  //document.querySelector(".wind").innerHTML =`${wDirection(response.data.wind.deg)} Wind:${Math.round(response.data.wind.speed)}mph`
   //;Gusts:${Math.round(response.data.wind.gust)}mph`;
  
-  document.querySelector("#current-time p").innerHTML = `Last updated: ${formatDate(response.data.dt*1000)} ${formatHours(response.data.dt*1000)} for`;
-  icon.setAttribute("src",`http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
-  );
-  icon.setAttribute("alt", response.data.weather[0].description);
-}
+  //document.querySelector("#current-time p").innerHTML = `Last updated: ${formatDate(response.data.dt*1000)} ${formatHours(response.data.dt*1000)} for`;
+  //icon.setAttribute("src",`http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  //);
+  //icon.setAttribute("alt", response.data.weather[0].description);
+//}
 function displayForecast(response) {
   let forecastElement = document.querySelector("#forecast");
   forecastElement.innerHTML = null;
@@ -106,7 +106,6 @@ function displayForecast(response) {
     </div>
   `;
   }
-
 }
 
 
@@ -114,7 +113,7 @@ function searchCity(city) {
   let apiKey = "cfeef4d0b0e86d888145aca4caf511b0";
   let units = "imperial";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
-  axios.get(apiUrl).then(showTemperature);
+  axios.get(apiUrl).then(showCoordinates);
 
   apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=${units}`;
   axios.get(apiUrl).then(displayForecast);
@@ -123,25 +122,41 @@ function searchCity(city) {
 
 function city(event) {
   event.preventDefault();
+
   let cityStateInput = document.querySelector("#location");
   let apiKey = "cfeef4d0b0e86d888145aca4caf511b0";
   let units = "imperial";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityStateInput.value}&appid=${apiKey}&units=${units}`;
-  axios.get(apiUrl).then(showTemperature);
+  axios.get(apiUrl).then(showCoordinates);
 
   apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${cityStateInput.value}&appid=${apiKey}&units=${units}`;
   axios.get(apiUrl).then(displayForecast);
 
-  //apiUrl = `https://api.openweathermap.org/data/2.5/uvi?lat=${lat}&lon=${long}&appid=${apiKey2}&units=${units2}`;
-  //axios.get(apiUrl2).then(displayUV);
-
 }
 
-document.querySelector("form.byCity").addEventListener("submit", city);
 
 //current Location
 
+function showCoordinates(response) {
+  let long = response.data.coord.lon;
+  let lat = response.data.coord.lat;
+  let units2 = "imperial";
+  let apiKey2 = "cfeef4d0b0e86d888145aca4caf511b0";
+
+  apiUrl2 = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${apiKey2}&units=${units2}`;
+  axios.get(apiUrl2).then(showTemperature2);
+
+  apiUrl2 = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${long}&appid=${apiKey2}&units=${units2}`;
+  axios.get(apiUrl2).then(displayForecast);
+
+  apiUrl2 = `https://api.openweathermap.org/data/2.5/uvi?lat=${lat}&lon=${long}&appid=${apiKey2}&units=${units2}`;
+  axios.get(apiUrl2).then(displayUV);
+}
+
 function showPosition(position) {
+       fahrenheitLink.classList.add("active");
+      celsiusLink.classList.remove("acitve");
+
   let lat = position.coords.latitude;
   let long = position.coords.longitude;
   let units2 = "imperial";
@@ -155,22 +170,29 @@ function showPosition(position) {
 
   apiUrl2 = `https://api.openweathermap.org/data/2.5/uvi?lat=${lat}&lon=${long}&appid=${apiKey2}&units=${units2}`;
   axios.get(apiUrl2).then(displayUV);
-
+}
   function showTemperature2(response) {
+      fahrenheitLink.classList.add("active");
+      celsiusLink.classList.remove("acitve");
   tempF = response.data.main.temp;
   tempFfeelsLike=response.data.main.feels_like;
-  console.log(response.data.wind);
-  console.log(response.data.coord.lon);
+  //console.log(response.data.wind);
+  //console.log(response.data.coord.lon);
 
-  //console.log(formatHours(response.data.sys.sunrise));
   document.querySelector(".currentTemp").innerHTML = `${Math.round(tempF)}`;
   document.querySelector(".currentCity").innerHTML = `${response.data.name}`;
-  document.querySelector("#feelsLike").innerHTML = `${Math.round(response.data.main.feels_like)}`;
+  document.querySelector("#location").value =`${response.data.name}`;
+
+
+  document.querySelector("#feelsLike").innerHTML = `${Math.round(tempFfeelsLike)}`;
   document.querySelector(".humidity").innerHTML = `${response.data.main.humidity}% Humidity`;
   document.querySelector(".sun").innerHTML =`Sunrise:${formatHours(response.data.sys.sunrise * 1000)} Sunset:${formatHours(response.data.sys.sunset*1000)}`;
   document.querySelector(".sky").innerHTML=response.data.weather[0].description;
   document.querySelector(".wind").innerHTML =`${wDirection(response.data.wind.deg)} Wind:${Math.round(response.data.wind.speed)}mph`;
   //Gusts:${Math.round(response.data.wind.gust)}mph`; not available
+  document.querySelector("#current-time p").innerHTML = `Last updated: ${formatDate(response.data.dt*1000)} ${formatHours(response.data.dt*1000)} for`;
+
+
   icon.setAttribute("src",`http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
   icon.setAttribute("alt", response.data.weather[0].description);
@@ -178,18 +200,13 @@ function showPosition(position) {
 
 //UV for current location
 function displayUV(response) {
-  console.log(response.data.value)
+ // console.log(response.data.value)
   document.querySelector(".uV").innerHTML=`UV Index:${Math.round(response.data.value)}`;
 
  // document.querySelector(".humidity").innerHTML = `${response.data.main.humidity}% Humidity`;
-
 }
 
-}
 
-function getCurrentPosition() {
-  navigator.geolocation.getCurrentPosition(showPosition);
-}
 //switch from F to C links
 function displayCelsiusTemperature(event) {
   event.preventDefault()
@@ -197,7 +214,6 @@ function displayCelsiusTemperature(event) {
   let tempCfeelsLike = (tempFfeelsLike -32)/1.8;
    celsiusLink.classList.add("active");
   fahrenheitLink.classList.remove("active");
-
   document.querySelector(".currentTemp").innerHTML = Math.round(tempC);
   document.querySelector("#feelsLike").innerHTML =Math.round(tempCfeelsLike);
 }
@@ -208,15 +224,10 @@ function displayFahrenheitTemperature(event) {
   fahrenheitLink.classList.add("active");
   document.querySelector(".currentTemp").innerHTML = Math.round(tempF);
 document.querySelector("#feelsLike").innerHTML =Math.round(tempFfeelsLike);
-
 } 
 
 let tempF=null;
 let tempFfeelsLike = null;
-let tempFforecastmin = null;
-
-let locate = document.querySelector("#currentlocation");
-locate.addEventListener("click", getCurrentPosition);
 
 let fahrenheitLink = document.querySelector("#fahrenheit-link");
 fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
@@ -224,5 +235,14 @@ fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
 let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
+function getCurrentPosition() {
+  navigator.geolocation.getCurrentPosition(showPosition);
+}
+
+document.querySelector("form.byCity").addEventListener("submit", city);
+
 searchCity("New York");
+
+let locate = document.querySelector("#currentlocation");
+locate.addEventListener("click", getCurrentPosition);
 
